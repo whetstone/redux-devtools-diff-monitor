d73399494bbe40ad2c7e2d0b4a263c1eda373403
Isaac Luxford <ikt.luxford@gmail.com>
2015-09-21 16:49:26 -0400

    adding a shortcut prop to DevTools component will set the shortcut to show diff monitor, defaults to ctrl+h, resolves #6

--- examples/todomvc/containers/App.js
+++ examples/todomvc/containers/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
         <Provider store={store}>
           {() => <TodoApp /> }
         </Provider>
-        <DevTools store={store} monitor={DiffMonitor} />
+        <DevTools store={store} monitor={DiffMonitor} shortcut='ctrl+d' />
     </div>
     );
   }
