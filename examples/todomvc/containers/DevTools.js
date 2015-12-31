6a71d71e99b582d9f652d56a25eb09b835be6877
Greg Fiorentino <gtfiorentino@gmail.com>
2015-12-31 06:54:25 -0500

    point to diff monitor in example

--- examples/todomvc/containers/DevTools.js
+++ examples/todomvc/containers/DevTools.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
-import LogMonitor from 'redux-devtools-log-monitor';
+import DiffMonitor from 'redux-devtools-diff-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 
 export default createDevTools(
   <DockMonitor toggleVisibilityKey='ctrl-h'
                changePositionKey='ctrl-q'>
-    <LogMonitor />
+    <DiffMonitor />
   </DockMonitor>
 );
--- examples/todomvc/webpack.config.js
+++ examples/todomvc/webpack.config.js
@@ -1,46 +1,45 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  resolve: {
-    alias: {
-      'redux-devtools/lib': path.join(__dirname, '..', '..', 'src'),
-      'redux-devtools': path.join(__dirname, '..', '..', 'src'),
-      'react': path.join(__dirname, 'node_modules', 'react')
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './index',
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/',
     },
-    extensions: ['', '.js']
-  },
-  resolveLoader: {
-    'fallback': path.join(__dirname, 'node_modules')
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      exclude: /node_modules/,
-      include: __dirname
-    }, {
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, '..', '..', 'src')
-    }, {
-      test: /\.css?$/,
-      loaders: ['style', 'raw'],
-      include: __dirname
-    }]
-  }
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    resolve: {
+        alias: {
+            'redux-devtools-diff-monitor': path.join(__dirname, '..', '..', 'lib'),
+            'react': path.join(__dirname, 'node_modules', 'react'),
+        },
+        extensions: ['', '.js'],
+    },
+    resolveLoader: {
+        'fallback': path.join(__dirname, 'node_modules'),
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['react-hot', 'babel'],
+            exclude: /node_modules/,
+            include: __dirname,
+        }, {
+            test: /\.js$/,
+            loaders: ['react-hot', 'babel'],
+            include: path.join(__dirname, '..', '..', 'src'),
+        }, {
+            test: /\.css?$/,
+            loaders: ['style', 'raw'],
+            include: __dirname,
+        }],
+    },
 };
