e6523d1c8fe2c8f3fb801b988b4c8b9375df1c6b
Isaac K T Luxford <ikt.luxford@gmail.com>
2016-02-05 16:43:47 -0500

    add counter example

---
+++ examples/counter/webpack.config.js
@@ -0,0 +1,40 @@
+var path    = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool      : 'eval',
+  entry        : [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output       : {
+    path      : path.join(__dirname, 'dist'),
+    filename  : 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins      : [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve      : {
+    alias: {
+      'redux-devtools-diff-monitor': path.join(__dirname, '..', '..', 'lib'),
+      'react'                      : path.join(__dirname, 'node_modules', 'react')
+    }
+  },
+  resolveLoader: {
+    'fallback': path.join(__dirname, 'node_modules')
+  },
+  module       : {
+    loaders: [{
+      test   : /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      exclude: /node_modules/,
+      include: path.join(__dirname, 'src')
+    }, {
+      test   : /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, '..', '..', 'src')
+    }]
+  }
+};
