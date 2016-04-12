e607df9ef2b1d10465f7ad2d1f507897ae180b07
Isaac Luxford <ikt.luxford@gmail.com>
2016-04-12 09:02:56 -0400

    template strings

--- src/action/index.js
+++ src/action/index.js
@@ -94,7 +94,7 @@ class ManifestActionComponent extends React.Component {
 
     const changesToStore = (expanded && storeHasChanged) ?
       <div
-        className={css(styles.code) + ' diff'}
+        className={`${css(styles.code)} diff`}
       >
         {changes}
       </div> : null;
@@ -102,7 +102,7 @@ class ManifestActionComponent extends React.Component {
     const enableToggle = skipped ? 'enable' : 'disable';
 
     return (
-      <div className={css(styles.container) + ' manifest-action-component'}>
+      <div className={`${css(styles.container)} manifest-action-component`}>
         <div>
           <div className={css(
             styles.header,
