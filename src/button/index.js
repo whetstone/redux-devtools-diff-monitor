c97c266242a9c77299a831e3ae3b2dc6cef610fc
Isaac Luxford <iluxford@sevone.com>
2016-04-12 08:44:57 -0400

    test updated, WIP

--- src/button/index.js
+++ src/button/index.js
@@ -1,7 +1,7 @@
-import React, {PropTypes} from 'react';
-import {StyleSheet, css} from 'aphrodite';
+import React, { PropTypes } from 'react';
+import { StyleSheet, css } from 'aphrodite';
 
-const ManifestButton = ({label, action}) => (
+const ManifestButton = ({ label, action }) => (
   <div className={css(styles.button)} onClick={action}>
     {label}
   </div>
@@ -9,16 +9,16 @@ const ManifestButton = ({label, action}) => (
 
 const styles = StyleSheet.create({
   button: {
-    flex      : '1 0 auto',
-    cursor    : 'pointer',
-    color     : 'black',
-    display   : 'inline-block',
-    padding   : '10px 0',
-    textAlign : 'center',
-    ':hover': {
-      backgroundColor: '#eee'
-    }
-  }
+    flex     : '1 0 auto',
+    cursor   : 'pointer',
+    color    : 'black',
+    display  : 'inline-block',
+    padding  : '10px 0',
+    textAlign: 'center',
+    ':hover' : {
+      backgroundColor: '#eee',
+    },
+  },
 });
 
 ManifestButton.propTypes = {
