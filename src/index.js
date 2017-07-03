0a51da1a8e5a1bb6b336c3b760cb61149cde4bc3
sowiecki <nase00@gmail.com>
2017-07-03 12:00:50 -0500

    Remove deprecated usage of React.PropTypes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ManifestAction from './action';
 import ManifestButton from './button';
 import diffState from './utils/diff-state';
